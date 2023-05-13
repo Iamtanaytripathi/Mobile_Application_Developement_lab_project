4e0a20fb72c728883f47570fc4810bd3ff79aeb2
BeyondSolar <abhicsjuet@gmail.com>
2023-05-13 09:47:17 +0530

    home_page 1.0 deleted unused files

--- focusSwipe/app/src/main/java/com/example/focusswipe/MainActivity.java
+++ focusSwipe/app/src/main/java/com/example/focusswipe/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.focusswipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, login_page.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
